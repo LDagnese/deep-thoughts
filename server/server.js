bc0df46db2018965a444b4d26fe3d26db49eee3a
loudagnese <louis.dagnese@gmail.com>
2022-10-08 20:04:19 -0700

    finish mutations and JWT auth

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 // import ApolloServer
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
+// import auth middleware
+const { authMiddleware } = require("./utils/auth");
 
 // import our typeDefs and resolvers
-const {typeDefs, resolvers} = require('./schemas');
+const { typeDefs, resolvers } = require("./schemas");
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
@@ -23,10 +26,10 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
-  server.applyMiddleware({app});
-}
+  server.applyMiddleware({ app });
+};
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
